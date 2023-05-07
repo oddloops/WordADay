627f44464ea632120e17f3dfcd1a636fff21d36a
Vinci Wu <wu.vinci00@gmail.com>
2023-05-06 21:50:30 -0400

    Hardcoded some words and made a Word class static

--- app/src/main/java/com/example/wordaday/WordActivity.java
+++ app/src/main/java/com/example/wordaday/WordActivity.java
@@ -29,10 +29,16 @@ public class WordActivity extends AppCompatActivity implements View.OnClickListe
         randomWordButton.setOnClickListener(this);
 
         // Add some Word objects to the ArrayList
-        words.add(new Word("apple", "/ˈæpəl/", "a round fruit with red or green skin and a white inside"));
-        words.add(new Word("banana", "/bəˈnænə/", "a long curved fruit with yellow skin and a soft inside"));
-        words.add(new Word("cherry", "/ˈtʃɛri/", "a small round fruit with a red or black skin and a stone inside"));
-        words.add(new Word("durian", "/ˈdʊriən/", "a large spiky fruit with a strong smell and a creamy inside"));
+        words.add(new Word("Discombobulate", "/dɪskəmˈbɒbjʊleɪt/", "to confuse or disconcert; upset; frustrate"));
+        words.add(new Word("Discombobulate", "/dɪskəmˈbɒbjʊleɪt/", "to confuse or disconcert; upset; frustrate"));
+        words.add(new Word("Ephemeral", "/ɪˈfɛmərəl/", "lasting a very short time; short-lived; transitory"));
+        words.add(new Word("Superfluous", "/suːˈpɜːflʊəs/", "being more than is sufficient or required; excessive; unnecessary"));
+        words.add(new Word("Conundrum", "/kəˈnʌndrəm/", "a riddle, the answer to which involves a pun or play on words; an enigma; a mystery"));
+        words.add(new Word("Ubiquitous", "/juːˈbɪkwɪtəs/", "present, appearing, or found everywhere; omnipresent"));
+        words.add(new Word("Solace", "/ˈsɒlɪs/", "comfort in sorrow, misfortune, or distress; consolation"));
+        words.add(new Word("Magnanimous", "/mæɡˈnænɪməs/", "generous in forgiving an insult or injury; unselfish; noble; high-minded"));
+        words.add(new Word("Esoteric", "/ˌɛsəʊˈtɛrɪk/", "intended for or likely to be understood by only a small number of people with a specialized knowledge or interest; obscure; arcane"));
+        words.add(new Word("Esoteric", "/ˌɛsəʊˈtɛrɪk/", "intended for or likely to be understood by only a small number of people with a specialized knowledge or interest; obscure; arcane"));
     }
 
     @Override
@@ -45,5 +51,29 @@ public class WordActivity extends AppCompatActivity implements View.OnClickListe
         wordTextView.setText(randomWord.getWord());
         pronunciationTextView.setText(randomWord.getPronunciation());
         definitionTextView.setText(randomWord.getDefinition());
+    }
+
+    public static class Word {
+        final String wordText;
+        final String pronunciation;
+        final String defintionText;
+
+        Word (String wordText, String pronunciation, String defintionText) {
+            this.wordText = wordText;
+            this.pronunciation = pronunciation;
+            this.defintionText = defintionText;
+        }
+
+        public String getWord() {
+            return this.wordText;
+        }
+
+        public String getPronunciation () {
+            return this.pronunciation;
+        }
+
+        public String getDefinition() {
+            return this.defintionText;
+        }
     }
 }
